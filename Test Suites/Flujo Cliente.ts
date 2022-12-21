8c4bd69f90feab1f1ff42557cdd41a608e687444
Bryan <barmas@olidario.fin.ec>
2022-12-21 13:34:59 -0500

    cambios en configuracion

--- Test Suites/Flujo Cliente.ts
+++ Test Suites/Flujo Cliente.ts
@@ -12,31 +12,10 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>80408658-2577-4550-af0c-7472074b8ecb</testSuiteGuid>
    <testCaseLink>
-      <guid>efd173f5-e413-4311-adc0-68fb0d37d80d</guid>
+      <guid>06dd0ca4-2c3c-4d09-861d-8baf7d891b65</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Cliente con usuario pero sin contraseña</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>db3e3147-3a34-450b-a10e-930ad12c916a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Inicio Flujo Campaña</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>ca406f5e-d2c3-45b0-b3ea-ccf1e856a661</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Flujo Clientes normal</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>1e6868f0-f3d0-493d-b659-e5c99a1a3b40</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Sin usuario y sin contraseña</testCaseId>
+      <testCaseId>Test Cases/Rapidito Cliente Validacion</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
